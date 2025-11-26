86feccb695a95e01263e3196e5867db7c8df9e02
Sergio Rafael <sergiorafaelgithub520@gmail.com>
2025-11-26 20:18:34 -0300

    Botão modo dark e light

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';//Eeduz erros silenciosos, melhora o desempenho, fornece mais avisos e menos recursos não seguros.
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+var className = document.body.className;
+if(className == "light-theme") {
+    this.textContent = "Dark";
+} else {
+    this.textContent = "Light";
+}
+
+console.log('current class name:' + className);
+})
